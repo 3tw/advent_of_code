681977510f447f7dffeb43690112696c43f32723
Teo Winkler <teo.winkler@gmail.com>
2022-12-03 19:52:00 +0100

    Improve day 3

--- src/days/day_3/mod.rs
+++ src/days/day_3/mod.rs
@@ -7,17 +7,14 @@ pub fn run() -> (String, String) {
 
     let sum_of_rucksack_items = include_str!("input.txt")
         .lines()
-        .map(|rucksack| {
-            let characters: Vec<&[u8]> = rucksack
+        .map(|line| line.split_at(line.len() / 2))
+        .map(|(left, right)| {
+            let common = left
                 .as_bytes()
-                .chunks(rucksack.as_bytes().len() / 2)
-                .collect();
-
-            // Get common element
-            let left = characters.get(0).unwrap();
-            let right = characters.get(1).unwrap();
-            let common = left.iter().find(|item| right.contains(item)).unwrap();
-
+                .iter()
+                .find(|item| right.as_bytes().contains(item))
+                .unwrap();
+                
             // Find its value in the normalized alphabet vector
             let item_value = alphabet.iter().position(|letter| letter == common).unwrap() + 1;
             return item_value as u32;
@@ -38,7 +35,11 @@ pub fn run() -> (String, String) {
         .map(|common| {
             let item_value = alphabet.iter().position(|letter| letter == common).unwrap() + 1;
             return item_value as u32;
-        }).sum::<u32>();
+        })
+        .sum::<u32>();
 
-    return (sum_of_rucksack_items.to_string(), sum_of_group_items.to_string());
+    return (
+        sum_of_rucksack_items.to_string(),
+        sum_of_group_items.to_string(),
+    );
 }
